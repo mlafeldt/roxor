c5132bef14fe32fc6c7266a813bede9f51ae91d7
Mathias Lafeldt <mathias.lafeldt@gmail.com>
2015-05-15 17:03:02 +0200

    Return first match using Option type

--- alt/roxor.rs
+++ alt/roxor.rs
@@ -6,6 +6,12 @@ use std::io::stderr;
 use std::path::Path;
 use std::process;
 
+struct Match {
+    offset: usize,
+    key: u8,
+    preview: String,
+}
+
 fn main() {
     let args: Vec<String> = env::args().skip(1).collect();
     if args.len() < 2 {
@@ -34,10 +40,16 @@ fn main() {
         process::exit(1);
     }
 
-    attack_cipher(ciphertext, crib);
+    match attack_cipher(ciphertext, crib) {
+        Some(m) => {
+            println!("Found text at 0x{:x} (XOR key 0x{:02x})", m.offset, m.key);
+            println!("  preview: {}", m.preview);
+        },
+        None => {},
+    };
 }
 
-fn attack_cipher(ciphertext: Vec<u8>, crib: Vec<u8>) {
+fn attack_cipher(ciphertext: Vec<u8>, crib: Vec<u8>) -> Option<Match> {
     for (i, x) in ciphertext.iter().enumerate() {
         let mut old_key = x ^ crib[0];
         let mut j = 1;
@@ -47,15 +59,14 @@ fn attack_cipher(ciphertext: Vec<u8>, crib: Vec<u8>) {
             old_key = key;
             j += 1;
             if j == crib.len() {
-                println!("Found text at 0x{:x} (XOR key 0x{:02x})", i, key);
-                print!("  preview: ");
                 let preview: String = ciphertext[i..i+50]
                     .iter()
                     .map(|x| x ^ key)
                     .map(|x| if x >= 32 && x <= 126 {x as char} else {'.'})
                     .collect();
-                println!("{}", preview);
+                return Some(Match { offset: i, key: key, preview: preview })
             }
         }
     }
+    return None
 }
